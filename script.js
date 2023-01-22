98af3f98ce0ddd31fb2876c0057f9b3e38c3e88a
J <118442144+model-map@users.noreply.github.com>
2023-01-22 15:48:19 +0530

    Play sound at key press.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 window.addEventListener('keydown',function(e){
-    console.log(e);
+    const key=document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    audio.currentTime=0.01;
+    audio.play();
 })
